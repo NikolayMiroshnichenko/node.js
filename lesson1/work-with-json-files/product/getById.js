8e2e25ca02f8cb74c181c4cfecb783939e721986
NikolayMiroshnichenkoUK <myroshnychenko.mykola@flawless.group>
2025-05-20 22:31:34 +0300

    README.md

--- lesson1/work-with-json-files/product/getById.js
+++
@@ -1,9 +0,0 @@
-const getAll = require("./getAll");
-
-const getById = async (id) => {
-    const products = await getAll();
-    const result = products.find(item => item.id === id);
-    return result || null;
-}
-
-module.exports = getById;  
--- lesson1/work-with-json-files/product/productPath.js
+++
@@ -1,5 +0,0 @@
-const path = require("path");
-const productPath = path.join(__dirname, "product.json");
-
-module.exports = productPath;
-
